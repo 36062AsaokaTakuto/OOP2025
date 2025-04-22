50f9d2d79714baf5b2871aed53f3f39138116cb0
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-04-22 10:25:54 +0900

    インスタンスメソッドとして利用

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -4,7 +4,7 @@ using System.Threading;
 namespace DistanceConverter {
 
     internal class Program {
-        
+
         //コマンドライン引数で指定された範囲のフィートとメートルの対応表を出力する
         static void Main(string[] args) {
 
@@ -20,25 +20,23 @@ namespace DistanceConverter {
 
         //フィートからメートルへの対応表を出力
         static void PrintFeetToMeterList(int start, int end) {
+            FeetConverter converter = new FeetConverter();
             for (int feet = start; feet <= end; feet++) {
                 //double meter = feet * 0.3048;
-                double meter = FeetConverter.ToMeter(feet);
+                double meter = converter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
         }
 
         //メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int start, int end) {
+            FeetConverter converter = new FeetConverter();
             for (int meter = start; meter <= end; meter++) {
                 //double meter = feet * 0.3048;
-                double feet = FeetConverter.FromMeter(meter);
+                double feet = converter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-
 
             }
         }
-
-
-
     }
 }
