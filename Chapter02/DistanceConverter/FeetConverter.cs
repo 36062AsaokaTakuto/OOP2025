c608e9ba8ca3602ddfa383fa9b14990439bddeea
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-04-22 11:12:08 +0900

    定数に修正

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
     public static class FeetConverter {
+
+        //定数
+        private const double retio = 0.3048;
         //メートルからフィートを求める
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / retio;
         }
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * retio;
         }
     }
 }
