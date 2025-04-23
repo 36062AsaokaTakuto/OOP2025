93a053239755d48768a04fca5be6a5878eb00265
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-04-23 10:40:44 +0900

    機能追加②

---
+++ Chapter02/演習問題/Exercize03/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercize02 {
+    internal class YardProgram {
+        static void Main(string[] args) {
+            Console.Write("変換前：");
+            int num = int.Parse(Console.ReadLine());//数字入力
+            Console.Write("変換：");
+            string henkan = Console.ReadLine();
+            if ("-tom" == henkan) {
+                PrintYardToMeterList(num);
+            } else {
+                PrintMeterToYardList(num);
+            }
+        }
+        // インチからメートルへの対応表を出力
+        static void PrintYardToMeterList(int num) {
+            int yard = num;
+            double meter = YardConverter.ToMeter(yard);
+            Console.WriteLine($"{yard}yard = {meter:0.000}m");
+
+        }
+
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToYardList(int num) {
+            int meter = num;
+            double yard = YardConverter.FromMeter(meter);
+            Console.WriteLine($"{meter}m = {yard:0.000}yard");
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercize03/ConsoleApp1/YardConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercize02{
+    class YardConverter{
+        //定数
+        private const double retio = 0.9144;
+        //ヤードからメートルを求める
+        public static double ToMeter(double yard) {
+            return yard * retio;
+        }
+
+        //メートルからヤードを求める
+        public static double FromMeter(double meter) {
+            return meter / retio;
+        }
+    }
+}
