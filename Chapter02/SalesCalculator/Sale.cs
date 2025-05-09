eb2408bb70b94f1480572b7e4838c4d2a77f4fcd
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-05-09 13:55:40 +0900

    売り上げ集計プログラム初版完成

---
+++ Chapter02/SalesCalculator/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator{
+    //売り上げクラス
+    public class Sale{
+        //店舗名
+        public string ShopName { get; set; } = string.Empty;//(= string.Empty)空を入れてる。("")と一緒
+        //商品カテゴリ
+        public string ProductCategory { get; set; } = string.Empty;
+        //売上高
+        public int Amount { get; set; }
+
+    }
+}
