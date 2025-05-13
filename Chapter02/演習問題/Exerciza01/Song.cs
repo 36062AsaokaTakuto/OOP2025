ac5e0106ffb21645923342ee12e24e4aa8adc76f
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-05-13 10:23:42 +0900

    2.1.2完成

--- Chapter02/演習問題/Exerciza01/Song.cs
+++ Chapter02/演習問題/Exerciza01/Song.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exerciza01{
+namespace Exerciza01 {
     //2.1.1
-    public class Song{
+    public class Song {
+        //歌のタイトル
         public string Title { get; set; } = string.Empty;
+        //アーティスト名
         public string ArtistName { get; set; } = string.Empty;
+        //演奏時間
         public int Length { get; set; }
-    }
+
+        //2.1.2
+        public Song (string title,string artistName,int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+    } 
+
 }
