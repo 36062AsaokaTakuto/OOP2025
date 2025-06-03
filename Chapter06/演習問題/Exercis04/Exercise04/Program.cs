f02181b58d1b025b4eb1db42162217320436cf71
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-06-03 10:51:10 +0900

    演習問題6.4答え

--- Chapter06/演習問題/Exercis04/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercis04/Exercise04/Program.cs
@@ -4,13 +4,25 @@ namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            ToJapanese(line);
-
-            var wards = line.Split(';');
-            foreach (var item in wards) {
-                var word = item.Split('=');
-                Console.WriteLine(ToJapanese(word[0]) + ':' + ToJapanese(word[1]));
+            //foreachバージョン
+            foreach (var pair in line.Split(';')) {
+                var word = pair.Split('=');
+                Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
             }
+
+            //for文バージョン
+            //var array = line.Split(';');
+            //for (int i = 0;i < array.Length; i++) {
+            //    var word = array[i].Split('=');
+            //    Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+            //}
+
+            //ToJapanese(line);
+            //var wards = line.Split(';');
+            //foreach (var item in wards) {
+            //    var word = item.Split('=');
+            //    Console.WriteLine(ToJapanese(word[0]) + ':' + ToJapanese(word[1]));
+            //}
 
         }
 
@@ -20,12 +32,18 @@ namespace Exercise04 {
         /// <param name="key">"Novelist","BestWork","Born"</param>
         /// <returns>"「作家」,「代表作」,「誕生年」</returns>
         static string ToJapanese(string key) {
-            switch (key) {
-                case "Novelist": return "作家";
-                case "BestWork": return "代表作";
-                case "Born": return "誕生年";
-                default: return key;//←どのcaseにも当てはまらない時に実行される処理
-            }
+            return key switch {
+                "Novelist" => "作家",
+                "BestWork" => "代表作",
+                "Born" => "誕生年",
+                _=> "引数keyは、正しい値ではありません"
+            };
+            //switch (key) {
+            //    case "Novelist": return "作家";
+            //    case "BestWork": return "代表作";
+            //    case "Born": return "誕生年";
+            //    default: return key;//←どのcaseにも当てはまらない時に実行される処理
+            //}
 
         }
     }
