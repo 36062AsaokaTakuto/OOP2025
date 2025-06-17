823df165258d1e6ce022290decb9008b7ad65a31
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-06-17 11:23:47 +0900

    設問１_Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; init; } = String.Empty;
+        public string Subject { get; init; } = String.Empty;
+        public int Score { get; init; }
+    }
+}
