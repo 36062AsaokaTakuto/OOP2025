d10aedfe4de82ed4bb3bbc4f96f4a5a580df1803
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-06-27 11:39:26 +0900

    9.2.1間違えてたから書き直した

--- Chapter09/演習問題/Exercize02/Program.cs
+++ Chapter09/演習問題/Exercize02/Program.cs
@@ -29,7 +29,7 @@ namespace Exercise02 {
         // 9.2.1【ここにプログラムを作成する】
         static DateTime NextWeek(DateTime date, DayOfWeek dayOfWeek) {
             var days = (int)dayOfWeek - (int)(date.DayOfWeek);
-            if (days <= 0) {
+            if (days <= 7) {
                 days += 7;
             }
             return date.AddDays(days);
