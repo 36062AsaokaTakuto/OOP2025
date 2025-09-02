e5a8ab50cb60e3705f85d545621bc19224c35767
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-09-02 13:23:14 +0900

    11章まで終了

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var strings = new[] {
+                "Microsoft Windows",
+                "windows",
+                "Windows Server",
+                "Windows",
+            };
+
+            var regex = new Regex(@"^(W|w)indows$");
+            var count = strings.Count(s => regex.IsMatch(s));
+            Console.WriteLine($"{count}行と一致");
+
+            //パターンと完全一致している文字列を出力する
+            strings.Where(s => regex.IsMatch(s)).ToList().ForEach(Console.WriteLine);
+
+
+
+        }
+    }
+}
---
+++ Chapter11/演習問題/Exercize02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercize02 {
+    internal class Program {
+        static void Main(string[] args) {
+            string filePath = "sample.txt";
+            Pickup3DigitNumber(filePath);
+        }
+
+        private static void Pickup3DigitNumber(string filePath) {
+            foreach (var line in File.ReadLines(filePath)) {
+                var matches = Regex.Matches(line,@"\b\d{3,}\b" );//\b = 境界
+                foreach (Match m in matches) {
+                    //結果を出力
+                    Console.WriteLine(m);
+                }
+            }
+        }
+    }
+}
