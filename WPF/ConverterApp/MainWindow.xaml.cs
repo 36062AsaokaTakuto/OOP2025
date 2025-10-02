16cbc3b1621aa4f4f25e13ba8f133e896ccf27a8
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-10-02 13:10:57 +0900

    ConverterAppいったん終わり

--- WPF/ConverterApp/MainWindow.xaml.cs
+++ WPF/ConverterApp/MainWindow.xaml.cs
@@ -18,11 +18,17 @@ namespace ConverterApp
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+        }
+
+        private void ImperialUnitToMetric_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void MetricToImperialUnit_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }
