a230bf83d22842a1050ff91ca11c5439a52c43ea
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-11-06 13:47:18 +0900

    URL表示完成

---
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        InitializeWebView();
+    }
+
+    private async void InitializeWebView() {
+        await WebView.EnsureCoreWebView2Async();
+    }
+
+
+    private void BackButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void ForwardButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void GoButton_Click(object sender, RoutedEventArgs e) {
+        WebView.Source = new Uri(AddressBar.Text);
+    }
+
+    
+}
