28520542dc9d94d3e3ec8e9dd9c6cd44f8f074d9
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-11-19 10:29:13 +0900

    15.1答え

--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,11 +1,11 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineOutputService();
+            var service = new LineToHalfNumberService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力：");
 
-            processor.Run(Console.ReadLine());
+            processor.Run(Console.ReadLine().Trim('"'));
         }
     }
 }
