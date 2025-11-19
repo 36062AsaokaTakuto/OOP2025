8deac1101b987b93150f2b8d43cb4f8bd8de488a
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-11-19 09:32:12 +0900

    問題15.3答え

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineToHalfNumberService : ITextFileService {
+        public void Initialize(string fname) {
+            
+        }
+
+        public void Execute(string line) {
+            
+        }
+
+        public void Terminate() {
+            
+        }
+    }
+}
