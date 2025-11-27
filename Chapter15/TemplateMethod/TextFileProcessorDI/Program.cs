abcf75b16e34cbccc30936b96d9919bf0ce2fbe3
AsaokaTakuto <oibc36062@ojs.ac.jp>
2025-11-27 15:25:45 +0900

    問題15.1完成

--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -4,7 +4,6 @@
             var service = new LineToHalfNumberService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力：");
-
             processor.Run(Console.ReadLine().Trim('"'));
         }
     }
